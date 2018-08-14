9fc1b18a80a2c5c95148758389a7f28354b617a2
kjazz <Krzysztof.jazgara@sonarsource.com>
2018-08-14 10:24:27 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World111");
         System.out.println("Hell");
-        System.out.println("Hellaaaaaa");
+        System.out.println("Hellaaaaaa");+
+        System.out.println("Nowy");    
     }
 
 }
